e47a9b60cb7137ec72297c44a82d467a7f793660
sako <sako@rio.ne.jp>
2017-04-26 21:52:42 +0900

    hoge

---
+++ selenium/firefox.rb
@@ -0,0 +1,31 @@
+class Firefox
+  require 'capybara'
+  require 'capybara/dsl'
+  require 'selenium-webdriver'
+
+  def start()
+    Capybara.current_driver = :selenium
+    Capybara.app_host = "https://affiliate.amazon.co.jp/"
+    Capybara.default_wait_time = 5
+    include Capybara::DSL
+
+    visit('')
+    # module Crawler
+    #     class Amazon
+    #         include Capybara::DSL
+    #
+    #         def login
+    #             visit('')
+    #             fill_in "username",
+    #             :with => 'YOUR_AMAZON_USER_ID'
+    #             fill_in "password",
+    #             :with => 'YOUR_AMAZON_PASSWORD'
+    #             click_button "サインイン"
+    #         end
+    #     end
+    # end
+    #
+    # crawler = Crawler::Amazon.new
+    # crawler.login
+  end
+end
