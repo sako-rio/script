c763fbfbdb0445e5b17b6d9bacdd9753b13fff7f
sako <sako@rio.ne.jp>
2017-04-29 08:58:40 +0900

    開始時刻を指定できるように修正

--- selenium/selenium.rb
+++ selenium/selenium.rb
@@ -4,14 +4,21 @@ require './chrome.rb'
 require './firefox.rb'
 require 'date'
 
+def timer(arg, &proc)
+  x = case arg
+  when Numeric then arg
+  when Time    then arg - Time.now
+  when String  then Time.parse(arg) - Time.now
+  else raise   end
+
+  sleep x if block_given?
+  yield
+end
+
 excelx = Excelx.new()
 order_information = excelx.purse()
-time_now = Time.now
-date_time = order_information['開始時間'].split('/')
-time = date_time.last.split(':')
-local_time = Time.new(date_time[0].to_i, date_time[1].to_i, date_time[2].to_i, time[0].to_i, time[1].to_i, time[2].to_i, "+09:00")
-# loop do
-#   break if local_time == time_now
-# end
-chrome = Chrome.new()
-chrome.start(order_information)
+
+timer(order_information['開始時間']) do
+  chrome = Chrome.new()
+  chrome.start(order_information)
+end
