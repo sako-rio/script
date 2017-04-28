a508b9e37f40fcc76d2aeeda82556c52e62614eb
sako <sako@rio.ne.jp>
2017-04-29 08:42:05 +0900

    復元

--- selenium/selenium.rb
+++ selenium/selenium.rb
@@ -10,8 +10,8 @@ time_now = Time.now
 date_time = order_information['開始時間'].split('/')
 time = date_time.last.split(':')
 local_time = Time.new(date_time[0].to_i, date_time[1].to_i, date_time[2].to_i, time[0].to_i, time[1].to_i, time[2].to_i, "+09:00")
-loop do
-  break if local_time == time_now
-end
+# loop do
+#   break if local_time == time_now
+# end
 chrome = Chrome.new()
-chrome.start()
+chrome.start(order_information)
