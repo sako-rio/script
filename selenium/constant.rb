922d7eab42e2cdb0a7b02cc7a8b1a2bd114fc151
sako <sako@rio.ne.jp>
2017-04-29 09:00:34 +0900

    コメント修正

--- selenium/constant.rb
+++ selenium/constant.rb
@@ -1,9 +1,9 @@
 #  定数ファイル
 
-#  動画と字幕の対応が記述してあるエクセルへのパス
+#  動画と字幕の対応が記述してあるエクセルへのパス(相対)
 $EXCELX = './botテンプレート.xlsx'
 $EXCELX.freeze
 
-#  メタムログイン画面へのパス
+#  シュプリームの購入サイトへのログ
 $URL = 'http://www.supremenewyork.com/shop/all'
 $URL.freeze
